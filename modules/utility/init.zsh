79e7b6716eb9e9ae0e1a81a8aaa622fc25736c29
Kasama <ralegro@bol.com.br>
2016-03-21 15:36:23 -0300

    add color always

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -71,7 +71,7 @@ if is-callable 'dircolors'; then
       eval "$(dircolors --sh)"
     fi
 
-    alias ls="${aliases[ls]:-ls} --color=auto"
+    alias ls="${aliases[ls]:-ls} --color=always"
   else
     alias ls="${aliases[ls]:-ls} -F"
   fi
@@ -107,7 +107,7 @@ if zstyle -t ':prezto:module:utility:grep' color; then
   export GREP_COLOR='37;45'           # BSD.
   export GREP_COLORS="mt=$GREP_COLOR" # GNU.
 
-  alias grep="${aliases[grep]:-grep} --color=auto"
+  alias grep="${aliases[grep]:-grep} --color=always"
 fi
 
 # Mac OS X Everywhere
